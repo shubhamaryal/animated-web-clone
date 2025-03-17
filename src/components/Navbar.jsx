af04d9f74d63ba96e6b0eb0bb3c9cbfe926e73e8
shubhamaryal <aryalshubham62@gmail.com>
2025-03-18 01:50:57 +0545

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
       transition-all duration-700 sm:inset-x-6"
     >
       <header className="absolute top-1/2 w-full -translate-y-1/2">
-        <nav className="flex size-full items-center justify-between">
+        <nav className="flex size-full items-center justify-between p-4">
           <div className="flex items-center gap-7">
             <img src="/img/logo.png" alt="logo" className="w-10" />
             <Button
