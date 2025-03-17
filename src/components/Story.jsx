85d603f2f13511584abd2fd95a3b85d5d89b00f6
shubhamaryal <aryalshubham62@gmail.com>
2025-03-18 01:18:24 +0545

    fix image of story section

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import AnimatedTitle from "./AnimatedTitle";
 import gsap from "gsap";
-
+import Button from "./Button";
 const Story = () => {
   const frameRef = useRef(null);
 
@@ -63,10 +63,54 @@ const Story = () => {
                   onMouseMove={handleMouseMove}
                   src="/img/entrance.webp"
                   alt="etrance"
-                  className="object-contai"
+                  className="object-contain"
                 />
               </div>
             </div>
+            {/* for the rounded corner */}
+            <svg
+              className="invisible absolute size-0"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <filter id="flt_tag">
+                  <feGaussianBlur
+                    in="SourceGraphic"
+                    stdDeviation="8"
+                    result="blur"
+                  />
+                  <feColorMatrix
+                    in="blur"
+                    mode="matrix"
+                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                    result="flt_tag"
+                  />
+                  <feComposite
+                    in="SourceGraphic"
+                    in2="flt_tag"
+                    operator="atop"
+                  />
+                </filter>
+              </defs>
+            </svg>
+          </div>
+        </div>
+
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p
+              className="mt-3 max-w-sm text-center font-circular-web text-violet-50
+            md:text-start"
+            >
+              Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+            <Button
+              id="realm-button"
+              title="discover prologue"
+              containerClass="mt-5"
+            />
           </div>
         </div>
       </div>
