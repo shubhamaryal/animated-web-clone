6a802e54dc99297bbdbdd109ab48d529abdda552
shubhamaryal <aryalshubham62@gmail.com>
2025-03-17 12:12:53 +0545

    make story section

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useRef } from "react";
+import AnimatedTitle from "./AnimatedTitle";
 
 const Story = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const frameRef = useRef(null);
 
-export default Story
+  const handleMouseLeave = () => {};
+
+  const handleMouseMove = (e) => {};
+
+  return (
+    <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          the multiversal ip world
+        </p>
+        <div className="relative size-full">
+          <AnimatedTitle
+            title="The st<b>o</b>ry of <br/> a hidden real<b>m</b>"
+            sectionId="#story"
+            containterClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
+                  src="/img/entrance.webp"
+                  alt="etrance"
+                  className="object-contai"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Story;
