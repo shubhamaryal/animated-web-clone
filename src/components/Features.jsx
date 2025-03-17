1b25c97eb68895064270927c1b14af1dd5b825cb
shubhamaryal <aryalshubham62@gmail.com>
2025-03-17 10:36:00 +0545

    make the cards in feature section animate on mouse hover

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { TiLocationArrow } from "react-icons/ti";
 
 const BentoTilt = ({ children, className = "" }) => {
-  return <div className={className}>{children}</div>;
+  const [transformStyle, setTransformStyle] = useState("");
+  const itemRef = useRef();
+
+  const handleMouseMove = (e) => {
+    if (!itemRef.current) return;
+    const { left, top, width, height } =
+      itemRef.current.getBoundingClientRect(); // get the position of the element
+
+    const relativeX = (e.clientX - left) / width;
+    const relativeY = (e.clientY - top) / height;
+
+    const titleX = (relativeX - 0.5) * 5;
+    const titleY = (relativeY - 0.5) * -5;
+
+    const newTransform = `perspective(700px) rotateX(${titleY}deg) rotateY(${titleX}deg) scale3d(0.98, 0.98, 0.98)`;
+
+    setTransformStyle(newTransform);
+  };
+
+  const handleMouseLeave = () => {
+    setTransformStyle("");
+  };
+
+  return (
+    <div
+      className={className}
+      ref={itemRef}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: transformStyle }}
+    >
+      {children}
+    </div>
+  );
 };
 
 const BentoCard = ({ src, title, description }) => {
